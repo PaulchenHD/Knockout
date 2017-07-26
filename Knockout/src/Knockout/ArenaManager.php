8dccc051b369804d0c1c8bdaf114aa630910a1fc
PaulchenHD <paulchen_hd@web.de>
2017-07-25 23:49:23 -1100

    Update ArenaManager.php

--- Knockout/src/Knockout/ArenaManager.php
+++ Knockout/src/Knockout/ArenaManager.php
@@ -4,7 +4,11 @@ namespace Knockout;
 
 class ArenaManager {
     public $active = [];
+    public $maxarenas = 10;
 
+    public function getMain(){
+        return new Main();
+    }
     public function isActive($name){
         if(isset($this->active[$name])){
             if($this->active[$name] == true){
@@ -17,6 +21,11 @@ class ArenaManager {
     public function setActive($name){
         if(!$this->isActive($name)){
             $this->active[$name] = true;
+        }
+    }
+    public function getAllActiveArenas(){
+        for($i=0; $i<$this->maxarenas; $i++){
+
         }
     }
 }
